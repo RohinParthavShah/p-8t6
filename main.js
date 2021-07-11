dfc7be6c8738635ab2466e86f245a2abd06b2bd1
RohinParthavShah <83176943+RohinParthavShah@users.noreply.github.com>
2021-07-11 09:25:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.Image.fromURL("BirthayImage.jpg",function(Img){
+        block_image_object= Img;
+        block_image_object.scaleToWidth(710);
+        block_image_object.scaleToHeight(410);
+        block_image_object.set({top:player_y,left:player_x});
+        canvas.add(block_image_object);
+    });
+}
+
+function playSound(){
+   x.play();
+}
