ba881bc4943a2a68738b3e5eb99bd9c612e7c12f
RohinParthavShah <83176943+RohinParthavShah@users.noreply.github.com>
2021-11-23 08:58:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function new_image()
         block_image_object= Img;
         block_image_object.scaleToWidth(710);
         block_image_object.scaleToHeight(410);
-        block_image_object.set({top:player_y,left:player_x});
+        block_image_object.set({top:0,left:0});
         canvas.add(block_image_object);
     });
 }
