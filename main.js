ba3504ae8be9d3a77c766290b508fa2c28695e1b
RohinParthavShah <83176943+RohinParthavShah@users.noreply.github.com>
2021-11-23 09:02:26 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function new_image()
 {
     fabric.Image.fromURL('BirthdayImage.jpg',function(Img){
         block_image_object= Img;
-        block_image_object.scaleToWidth(710);
-        block_image_object.scaleToHeight(410);
+        block_image_object.scaleToWidth(600);
+        block_image_object.scaleToHeight(400);
         block_image_object.set({top:0,left:0});
         canvas.add(block_image_object);
     });
